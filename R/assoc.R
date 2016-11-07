f98a914a7ff290ff0607029eacdafb46e09c08d6
Chris C <chris.c.1221@gmail.com>
2016-11-07 13:26:15 +0000

    Auto update GWAS_SIM, see ChangeLog for more details.

--- R/assoc.R
+++ R/assoc.R
@@ -1,8 +1,36 @@
 library(qqman)
+library(data.table)
 
-ceu <- read.table("output/ceu.qassoc", h = T)
-yri <- read.table("output/yri.qassoc", h = T)
+ceu <- fread("output/ceu.qassoc", h = T)
+yri <- fread("output/yri.qassoc", h = T)
+dataset <- fread("output/dataset.qassoc", h = T)
 
+snps <- read.table("lib/snplist.txt", h = F)
+snps <- snps$V1
 
+if(!dir.exists("diagnostics")) dir.create("diagnostics")
 
+png('diagnostics/ceu.man.png')
+manhattan(ceu, highlight = snps)
+dev.off()
+
+png('diagnostics/yri.man.png')
+manhattan(yri, highlight = snps)
+dev.off()
+
+png('diagnostics/dataset.man.png')
+manhattan(dataset, highlight = snps)
+dev.off()
+
+png('diagnostics/ceu.qq.png')
+qq(ceu$P)
+dev.off()
+
+png('diagnostics/yri.qq.png')
+qq(yri$P)
+dev.off()
+
+png('diagnostics/dataset.qq.png')
+qq(dataset$P)
+dev.off()
 
