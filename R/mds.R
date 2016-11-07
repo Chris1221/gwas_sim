0944c7faa24cdfa9791bd4dd5dc9826c158ce4f9
Chris C <chris.c.1221@gmail.com>
2016-11-07 16:29:13 +0000

    Auto update GWAS_SIM, see ChangeLog for more details.

--- R/mds.R
+++ R/mds.R
@@ -2,7 +2,7 @@ library(data.table)
 
 m <- as.matrix(fread("output/dataset.mibs"))
 mds <- cmdscale(as.dist(1-m))
-k <- c( rep("green",4500) , rep("blue",50) )
+k <- c( rep("green",4500) , rep("blue",500) )
 
 png('diagnostics/mds.png')
 plot(mds,pch=20,col=k)
